542961fa5cb95d95d6dfcda169cffe791b03522f
Bobbanz1 <59128051+Bobbanz1@users.noreply.github.com>
2023-01-16 11:05:44 +0100

    Update sketch_sep16a.ino

--- sketch_sep16a.ino
+++ sketch_sep16a.ino
@@ -1,3 +1,10 @@
+//DEFINES
+#define SECONDS *1000
+#define MINUTES SECONDS*60
+#define HOURS MINUTES*60
+#define DAYS HOURS*24
+
+//VARIABLES
 //Integer used to produce a readable output of what level of moisture the soil is currently at.
 int moist = 0;
 //Boolean used to determine if the plant has been watered or not according to the code.
@@ -7,6 +14,7 @@ bool uncertWater = false;
 //Change this to true if you're currently debugging the code and don't want to wait an entire day for the next watering.
 bool debug = true;
 
+//MAIN CODE
 void setup() {
   // put your setup code here, to run once:
   pinMode(A5, INPUT);
@@ -15,6 +23,7 @@ void setup() {
 
   moist = analogRead(A5);
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -28,23 +37,24 @@ void loop() {
   } else if (watered | uncertWater) {
     if (debug) {
       //Debug version of this to both showcase it works and to test stuff, will instead wait 3 seconds before repeating the loop
-      delay(3000);
+      delay(3 SECONDS);
 
       //Reset the variables as we are running in debug mode and there's a chance we might miss reseting them otherwise
       uncertWater = false;
       watered = false;
     } else if (uncertWater) {
-      //Delay by 12 Hours before repeating the loop as we are uncertain if the flower has actually been watered
-      delay(43200000);
+      //Delay by 12 Hours before repeating the loop as we are uncertain if the plant has actually been watered
+      delay(12 HOURS);
       uncertWater = false;
     } else {
       //If we have watered today then wait exactly 24 hours until we repeat this cycle.
-      delay(86400000);
+      delay(1 DAYS);
     }
     //It has been a day, the plant is checked if it needs watering yet again
     watered = false;
   }
 }
+
 
 void MoistChecking(int input) {
   if (input <= 50) {
@@ -52,13 +62,14 @@ void MoistChecking(int input) {
     digitalWrite(2, HIGH);
 
     //Wait for five seconds of continous watering before checking for errors
-    delay(5000);
+    delay(5 SECONDS);
     PowerControl(input);
   }
 }
 
+
 void PowerControl(int check) {
-  //Check to see if the system is telling us that there still hasn't done any watering, in which case we allow for an error of margin.
+  //Check to see if the system is telling us that there still hasn't been any watering done, in which case we allow for an error of margin.
   if (check <= 50) {
     //Terminates Ground connection to power source
     digitalWrite(2, LOW);
